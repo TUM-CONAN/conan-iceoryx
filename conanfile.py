115d92daa1e1b80b8a1e935119f58a0eacdd7894
Ulrich Eck <33796465+stytim@users.noreply.github.com>
2025-07-30 23:25:04 +0200

    fix build on windows

--- conanfile.py
+++ conanfile.py
@@ -51,9 +51,9 @@ class IceoryxConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def configure(self):
-        if self.options.shared:
-            del self.options.fPIC
+    #def configure(self):
+    #    if self.options.shared:
+    #        del self.options.fPIC
 
     def requirements(self):
         if self.options.toml_config:
